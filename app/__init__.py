b6d6edc5a3d8c06e63a833e4f5a9bece92257439
Harry Stringa <hwstringer@waimea.school.nz>
2025-05-16 15:05:40 +1200

    db connected

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,26 @@
 from flask import Flask
 from flask import render_template
-from random import randint
+from supabase import create_client
+from dotenv import load_dotenv
+import os
+
+#get environment variables
+load_dotenv()
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
+supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+print(supabase)
 
 app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return render_template("pages/home.jinja")
+    response = supabase.table("things").select().execute()
+    records = response.data
+
+    return render_template("pages/home.jinja", things=records)
 
 @app.errorhandler(404)
-def notFound():
+def notFound(errorhandler):
     return render_template("pages/404.jinja")
